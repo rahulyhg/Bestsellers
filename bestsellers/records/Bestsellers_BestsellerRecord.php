63a59b7ff3c5fc2b4790c6c9050b64e1d06b25cf
amogower <amogower@gmail.com>
2017-06-27 00:03:45 +0100

    Add create method for Bestseller record class

--- bestsellers/records/Bestsellers_BestsellerRecord.php
+++ bestsellers/records/Bestsellers_BestsellerRecord.php
@@ -3,11 +3,21 @@ namespace Craft;
 
 class Bestsellers_BestsellerRecord extends BaseRecord
 {
+    /**
+     * Gets the database table name
+     *
+     * @return string
+     */
     public function getTableName()
     {
         return 'bestsellers_bestsellers';
     }
 
+    /**
+     * Define columns for our database table
+     *
+     * @return array
+     */
     protected function defineAttributes()
     {
         return array(
@@ -15,10 +25,27 @@ class Bestsellers_BestsellerRecord extends BaseRecord
         );
     }
 
+    /**
+     * Define relations for our database table
+     *
+     * @return array
+     */
     public function defineRelations()
     {
         return array(
             'product' => array(static::HAS_ONE, 'Commerce_ProductRecord', 'productId'),
         );
+    }
+
+    /**
+     * Create a new instance of the current class
+     *
+     * @return BaseRecord
+     */
+    public function create()
+    {
+        $class = get_class($this);
+        $record = new $class();
+        return $record;
     }
 }
